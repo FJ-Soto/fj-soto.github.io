f15482f73115b04bc5e4ce70112851b4daa7f9db
Fernando Soto <60342586+fsoto-GH@users.noreply.github.com>
2024-12-22 03:20:01 -0600

    add favicons, justify content, resume information

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,7 @@
 const scrollNavigation = () => {
-  const style = window.getComputedStyle(document.body);
-  const scrollTriggerHeight = +style
-    .getPropertyValue("--nav-bar-height")
-    .slice(0, -2);
   const navBar = document.getElementById("nav-bar");
-  if (window.scrollY > scrollTriggerHeight) {
+  const navHeight = parseInt(getComputedStyle(navBar).height);
+  if (window.scrollY > navHeight) {
     navBar.classList.add("scrolled");
   } else {
     navBar.classList.remove("scrolled");
